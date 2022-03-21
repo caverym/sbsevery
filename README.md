4c2a697f67a07551ebc19c5be8035ddfd9798934
caverym <averylapine@gmail.com>
2022-03-21 10:38:43 -0700

    readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# Sbsevery
+
+Secure boot sign every(thing)
+
+Recursively sign files for secureboot (helpful when dualbooting Windows with custom SB keys)
+
+## Usage
+
+quietly sign all files
+```
+sbsevery /efi -k /etc/efi-keys/DB.key -c /etc/efi-keys/DB.crt
+```
+
+verbosley sign all filess
+```
+sbsevery /efi -k /etc/efi-keys/DB.key -c /etc/efi-keys/DB.crt -d
+```
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
+/*!
+ * # Sbsevery
+ * 
+ * Secure boot sign every(thing)
+ * 
+ * Recursively sign files for secureboot (helpful when dualbooting Windows with custom SB keys)
+ * 
+ * ## Usage
+ * 
+ * quietly sign all files
+ * ```
+ * sbsevery /efi -k /etc/efi-keys/DB.key -c /etc/efi-keys/DB.crt
+ * ```
+ * 
+ * verbosely sign all files
+ * ```
+ * sbsevery /efi -k /etc/efi-keys/DB.key -c /etc/efi-keys/DB.crt -d
+ * ```
+ */
+
 use std::{
     path::{Path, PathBuf},
     sync::Arc,
     thread::{self, JoinHandle},
 };
-
 
 use jargon_args::Jargon;
 
